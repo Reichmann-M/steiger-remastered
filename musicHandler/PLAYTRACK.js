a3fa6cc6e8987c1295b1652c314a95c298b8dbf7
Max Reichmann <max.reichmann@gmx.de>
2023-05-23 14:33:28 +0200

    Domain Driven Design Change 2

--- musicHandler/PLAYTRACK.js
+++ musicHandler/PLAYTRACK.js
@@ -8,103 +8,104 @@ const { MessageEmbed } = require('discord.js')
 const SpotifyYoutubeIntegrationJS = require('../include/SpotifyYoutubeIntegration')
 const failedFetchAutoRetry = require('../util/failedFetchAutoRetry')
 const play = module.exports = async (message) => {
-    if (message.guild.queue.length == 0) {
+    var discord_server = message.guild;
+    if (discord_server.queue.length == 0) {
         return;
     }
 
-    if (message.guild.dispatcher != undefined) {
-        if (message.guild.dispatcher.paused == true) {
+    if (discord_server.dispatcher != undefined) {
+        if (discord_server.dispatcher.paused == true) {
             return;
         }
     }
 
-    if (message.guild.nowPlaying) {
+    if (discord_server.nowPlaying) {
         return;
     }
-    // console.log(`Trying to play ${message.guild.queue[0].type}: ${message.guild.queue[0].title}`) // remove for production
+    // console.log(`Trying to play ${discord_server.queue[0].type}: ${discord_server.queue[0].title}`) // remove for production
 
-    if (!message.guild.usedVoiceChannel) {
-        message.guild.usedVoiceChannel = message.member.voice.channel
+    if (!discord_server.usedVoiceChannel) {
+        discord_server.usedVoiceChannel = message.member.voice.channel
     }
 
     // Declare stream variables
     var stream = null;
     var streamOptions = {}
 
-    if (message.guild.queue[0].type == 'YoutubeVideo' || message.guild.queue[0].type == 'YoutubeStream') {
+    if (discord_server.queue[0].type == 'YoutubeVideo' || discord_server.queue[0].type == 'YoutubeStream') {
         
-        stream = await failedFetchAutoRetry('ytdl', message.guild.queue[0].playLink)
+        stream = await failedFetchAutoRetry('ytdl', discord_server.queue[0].playLink)
         if (stream == null) {
-            console.log(`[🎩${message.client.bot_config_number}]❗ Unable to play track |${message.guild.queue[0].playLink}|`)
+            console.log(`[🎩${message.client.bot_config_number}]❗ Unable to play track |${discord_server.queue[0].playLink}|`)
             message.channel.send(new MessageEmbed({
                 color: "#e60000",
-                description: `❗ Video **${message.guild.queue[0].title}** nicht verfügbar [3 failed attempts to fetch video stream]`
+                description: `❗ Video **${discord_server.queue[0].title}** nicht verfügbar [3 failed attempts to fetch video stream]`
             }))
-            message.guild.queue.shift()
-            message.guild.dispatcher = null;
-            message.guild.nowPlaying = false
-            message.guild.isLooped = false;
-            message.guild.me.setNickname("");
+            discord_server.queue.shift()
+            discord_server.dispatcher = null;
+            discord_server.nowPlaying = false
+            discord_server.isLooped = false;
+            discord_server.me.setNickname("");
             return play(message);
         }       
         
         streamOptions = {
             type: 'opus'
         }
-    } else if (message.guild.queue[0].type == 'Soundcloud') {
+    } else if (discord_server.queue[0].type == 'Soundcloud') {
 
-        stream = await scdl.download(message.guild.queue[0].playLink, 'gi3TTq7WE8J3QwqBwG5PaY639Ufl7RLH')
+        stream = await scdl.download(discord_server.queue[0].playLink, 'gi3TTq7WE8J3QwqBwG5PaY639Ufl7RLH')
         streamOptions = {
             type: 'unknown'
         }
-    } else if (message.guild.queue[0].type == 'WebStream') {
-        stream = message.guild.queue[0].playLink,
+    } else if (discord_server.queue[0].type == 'WebStream') {
+        stream = discord_server.queue[0].playLink,
             streamOptions = {
                 type: 'unknown'
             }
-    } else if (message.guild.queue[0].type == 'SpotifyItem') {
+    } else if (discord_server.queue[0].type == 'SpotifyItem') {
 
-        // console.log(`Trying to play: ${message.guild.queue[0].title}`) // remove for production
-        await SpotifyYoutubeIntegrationJS.handle(message, message.guild.queue[0])
-        if (message.guild.queue[0] == null) {
-            message.guild.queue.shift();
+        // console.log(`Trying to play: ${discord_server.queue[0].title}`) // remove for production
+        await SpotifyYoutubeIntegrationJS.handle(message, discord_server.queue[0])
+        if (discord_server.queue[0] == null) {
+            discord_server.queue.shift();
             stream = null;
-            message.guild.dispatcher = null;
-            message.guild.nowPlaying = false
-            message.guild.isLooped = false;
-            message.guild.me.setNickname("");
+            discord_server.dispatcher = null;
+            discord_server.nowPlaying = false
+            discord_server.isLooped = false;
+            discord_server.me.setNickname("");
         }
         return play(message)
     }
 
 
-    console.log(`[🎩${message.client.bot_config_number}]✅ Playing ${message.guild.queue[0].title} (${message.guild.queue[0].playLink}) to voicechannel ${message.guild.usedVoiceChannel.name} (${message.guild.usedVoiceChannel.id})`)
-    message.guild.connection = await message.guild.usedVoiceChannel.join()
+    console.log(`[🎩${message.client.bot_config_number}]✅ Playing ${discord_server.queue[0].title} (${discord_server.queue[0].playLink}) to voicechannel ${discord_server.usedVoiceChannel.name} (${discord_server.usedVoiceChannel.id})`)
+    discord_server.connection = await discord_server.usedVoiceChannel.join()
     
     // Bot selfdeafens and serverdeafens itself
     try {
-        message.guild.connection.voice.setSelfDeaf(true)
-        message.guild.connection.voice.setDeaf(true)
+        discord_server.connection.voice.setSelfDeaf(true)
+        discord_server.connection.voice.setDeaf(true)
     } catch (error) {
         // console.log(error) // remove for production
     }
     
     
-    message.guild.dispatcher = message.guild.connection
+    discord_server.dispatcher = discord_server.connection
         .play(stream, streamOptions)
         // .play(stream)
         .on("error", async (err) => {
             console.log(err)
-            message.guild.queue.shift()
+            discord_server.queue.shift()
             stream = null;
-            message.guild.dispatcher = null;
-            message.guild.nowPlaying = false
-            message.guild.isLooped = false;
-            message.guild.loopCount = 0
-            message.guild.me.setNickname("");
+            discord_server.dispatcher = null;
+            discord_server.nowPlaying = false
+            discord_server.isLooped = false;
+            discord_server.loopCount = 0
+            discord_server.me.setNickname("");
             try {
-                await message.guild.playingMessage.delete()
-                message.guild.playingMessage = null;
+                await discord_server.playingMessage.delete()
+                discord_server.playingMessage = null;
                 stream.destroy()
             } catch (error) {
                 // console.log(error) // remove for production
@@ -112,35 +113,35 @@ const play = module.exports = async (message) => {
             return play(message);
         })
         .on("finish", async () => {
-            message.guild.nowPlaying = false
+            discord_server.nowPlaying = false
             stream = null;
-            message.guild.dispatcher = null;
-            message.guild.me.setNickname("");
+            discord_server.dispatcher = null;
+            discord_server.me.setNickname("");
             try {
-                await message.guild.playingMessage.delete()
-                message.guild.playingMessage = null;
+                await discord_server.playingMessage.delete()
+                discord_server.playingMessage = null;
                 stream.destroy()
             } catch (error) {
                 // console.log(error) // Can be removed for production
             }
-            if (!(message.guild.isLooped)) {
-                message.guild.queue.shift()
+            if (!(discord_server.isLooped)) {
+                discord_server.queue.shift()
                 return play(message);
             } else {
                 // LOOP Activated
-                message.guild.loopCount = message.guild.loopCount + 1
+                discord_server.loopCount = discord_server.loopCount + 1
                 return play(message);
             }
         })
 
     // Maximum Audio Quality
-    message.guild.dispatcher.setBitrate('auto')
-    message.guild.dispatcher.setVolumeLogarithmic(message.guild.volume / 100)
+    discord_server.dispatcher.setBitrate('auto')
+    discord_server.dispatcher.setVolumeLogarithmic(discord_server.volume / 100)
 
 
-    message.guild.nowPlaying = true;
+    discord_server.nowPlaying = true;
 
-    message.guild.me.setNickname(`🔊 ${message.client.user.username}`);
+    discord_server.me.setNickname(`🔊 ${message.client.user.username}`);
     const PlayingMessageJS = require('../util/playingMessage')
     await PlayingMessageJS.update(message)
 }
